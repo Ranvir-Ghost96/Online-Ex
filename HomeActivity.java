a64d3687d31ddaaab65a5145a6b104af56ee72c7
Ranvir-Ghost96 <136616421+Ranvir-Ghost96@users.noreply.github.com>
2023-06-15 13:27:27 +0530

    Add files via upload

---
+++ HomeActivity.java
@@ -0,0 +1,52 @@
+package com.example.online_ex;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.online_ex.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        Toolbar toolbar =(Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Home");
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab =(FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action",null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+}
---
+++ Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.online_ex.Prevalent;
+
+import com.example.online_ex.Model.Users;
+
+public class Prevalent {
+    private static Users currentOnlineUser;
+
+    public static final String UserPhoneKey ="UserPhone";
+    public static final String UserPasswordKey ="UserPassword";
+}
